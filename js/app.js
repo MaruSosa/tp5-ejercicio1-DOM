8b4c9a3ba4d126992d0d6ca8e7de2ab5f6055b28
mariana <marianass712@gmail.com>
2023-11-23 20:52:46 -0300

    se realizo la logica de comparacion de numerosy se hizo las funciones de los botones de comenzar el juego y enviar el numero ingresado

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
-
 let inputAgregado = false;
 const comenzarJuego = () => {
   if (!inputAgregado) {
-    const texto = document.createElement('p');
-    texto.innerHTML= 'Una pista..el número oculto esta entre 0 y 100!!'
-    const inputNumero = document.createElement('input');
-    inputNumero.setAttribute('type', 'number');
-    inputNumero.setAttribute('placeholder', 'Ingresa un número');
-    const submitButton = document.getElementById('submitButton');
-    const contenedor = document.querySelector('.ingresoNumero');
+    const texto = document.createElement("p");
+    texto.innerHTML = "Una pista..el número oculto se encuentra entre 0 y 100!!";
+    const inputNumero = document.createElement("input");
+    inputNumero.setAttribute("type", "number");
+    inputNumero.setAttribute("placeholder", "Ingresa un número");
+    const submitButton = document.createElement("button");
+    submitButton.innerHTML = "Enviar";
+    const contenedor = document.querySelector(".ingresoNumero");
     contenedor.appendChild(texto);
     contenedor.appendChild(inputNumero);
-    inputAgregado = true; 
+    contenedor.appendChild(submitButton);
+    inputAgregado = true;
+    numeroAleatorio = crearNumeroAleatorio();
+    submitButton.addEventListener("click", comparacionNumeros);
   }
-  const bienvenida = document.getElementById('bien');
-  const startButton = document.getElementById('startButton');
-  const inputNumeroNuevo = document.getElementById('idInput');
-  bienvenida.style.display = 'none'; 
-  startButton.style.display = 'none';
-  inputNumero.style.display = 'block';
+  const bienvenida = document.getElementById("bien");
+  const startButton = document.getElementById("startButton");
+  const inputNumeroNuevo = document.getElementById("idInput");
+  bienvenida.style.display = "none";
+  startButton.style.display = "none";
+  inputNumero.style.display = "block";
 };
-document.getElementById('startButton').addEventListener('click', comenzarJuego);
-const crearNumeroAleatorio=()=>{
+document.getElementById("startButton").addEventListener("click", comenzarJuego);
+const crearNumeroAleatorio = () => {
+  const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+  return numeroAleatorio;
+};
 
-}
+const comparacionNumeros = () => {
+  const numeroIngresado = parseInt(document.querySelector('input[type="number"]').value);
+  if (numeroAleatorio === numeroIngresado) {
+    alert("¡Felicidades! Has adivinado el número.");
+  } else {
+    alert("Intenta de nuevo. El número no coincide.");
+  }
+  document.querySelector('input[type="number"]').value='';
+};
+
+
