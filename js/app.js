0e03a6fde8627ec3e49b729d5a8fc675d21ed42f
mariana <marianass712@gmail.com>
2023-11-23 18:23:58 -0300

    se hizo la logica de que al presionar comenzar juego aparezca un mensaje y un input

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 
-// let inputAgregado = false;
-// const comenzarJuego = () => {
-//   if (!inputAgregado) {
-//     const texto = document.createElement('p');
-//     texto.innerHTML= 'Una pista..el número oculto esta entre 0 y 100!!'
-//     const inputNumero = document.createElement('input');
-//     inputNumero.setAttribute('type', 'number');
-//     inputNumero.setAttribute('placeholder', 'Ingresa un número');
-//     const submitButton = document.getElementById('submitButton');
-//     const contenedor = document.querySelector('.ingresoNumero');
-//     contenedor.appendChild(texto);
-//     contenedor.appendChild(inputNumero);
-//     inputAgregado = true; 
-//   }
-//   const bienvenida = document.getElementById('bien');
-//   const startButton = document.getElementById('startButton');
-//   const inputNumeroNuevo = document.getElementById('idInput');
-//   bienvenida.style.display = 'none'; 
-//   startButton.style.display = 'none';
-//   inputNumero.style.display = 'block';
-// };
-// document.getElementById('startButton').addEventListener('click', comenzarJuego);
-// const crearNumeroAleatorio=()=>{
+let inputAgregado = false;
+const comenzarJuego = () => {
+  if (!inputAgregado) {
+    const texto = document.createElement('p');
+    texto.innerHTML= 'Una pista..el número oculto esta entre 0 y 100!!'
+    const inputNumero = document.createElement('input');
+    inputNumero.setAttribute('type', 'number');
+    inputNumero.setAttribute('placeholder', 'Ingresa un número');
+    const submitButton = document.getElementById('submitButton');
+    const contenedor = document.querySelector('.ingresoNumero');
+    contenedor.appendChild(texto);
+    contenedor.appendChild(inputNumero);
+    inputAgregado = true; 
+  }
+  const bienvenida = document.getElementById('bien');
+  const startButton = document.getElementById('startButton');
+  const inputNumeroNuevo = document.getElementById('idInput');
+  bienvenida.style.display = 'none'; 
+  startButton.style.display = 'none';
+  inputNumero.style.display = 'block';
+};
+document.getElementById('startButton').addEventListener('click', comenzarJuego);
+const crearNumeroAleatorio=()=>{
 
-// }
+}
